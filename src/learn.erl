a95547c74fcd61e610b8f00b789cd1d7f84ab645
Jesse Gumm <sigmastar@gmail.com>
2013-08-18 13:33:39 -0500

    Update learn more page (width=80, add plugin text)

--- src/learn.erl
+++ src/learn.erl
@@ -37,81 +37,98 @@ left() ->
         #h2 { text="Event-Driven Development" },
         #p{},
         "
-        Nitrogen uses an event-driven model built on top of Erlang pattern matching. Nitrogen
-        allows you to tag elements with any Erlang term, and then act on the tag in server-side
-        code when the user clicks on, hovers over, or otherwise interacts with the element. 
-        Catching the event is as simple as writing an Erlang function.
+        Nitrogen uses an event-driven model built on top of Erlang pattern
+        matching. Nitrogen allows you to tag elements with any Erlang term, and
+        then act on the tag in server-side code when the user clicks on, hovers
+        over, or otherwise interacts with the element.  Catching the event is as
+        simple as writing an Erlang function.
         ",
         
         #h2 { text="Brainlessly Easy Ajax" },
         #p{},
         "
-        Nitrogen allows you to update or replace a section of your page using Ajax in just one 
-        line of code. Most importantly, Nitrogen lets you use the same consistent syntax to both
-        build AND update the page.
+        Nitrogen allows you to update or replace a section of your page using
+        Ajax in just one line of code. Most importantly, Nitrogen lets you use
+        the same consistent syntax to both build AND update the page.
         ",
         
         #h2 { text="Ridiculously Simple Comet" },
         #p{},
         "
-        Nitrogen includes Comet support, allowing you to build interactive web applications
-        that push data to the browser. By simply wrapping your function call
-        in one line of code, you can turn a synchronous function into a long-running asynchronous function.
+        Nitrogen includes Comet support, allowing you to build interactive web
+        applications that push data to the browser. By simply wrapping your
+        function call in one line of code, you can turn a synchronous function
+        into a long-running asynchronous function.
         ",
         
         #h2 { text="Complex Interfaces: Drag/Drop/Sort" },
         #p{},
         "
-        With Nitrogen, you can let a user interact with your application in complex ways.
-        Nitrogen lets you tag the draggable, droppable, or sortable elements, and then respond to 
-        the resulting events in server-side code as easily as you would respond to a click. 
+        With Nitrogen, you can let a user interact with your application in
+        complex ways.  Nitrogen lets you tag the draggable, droppable, or
+        sortable elements, and then respond to the resulting events in
+        server-side code as easily as you would respond to a click. 
         ",
         
         #h2 { text="Flexible Templating" },
         #p{},
         "
-        Nitrogen includes a simple but powerful template system, allowing you to define
-        a consistent style for your application. You can add headers, footers, and parameterized 
-        plugins to your page using a simple callout mechanism.
+        Nitrogen includes a simple but powerful template system, allowing you
+        to define a consistent style for your application. You can add headers,
+        footers, and parameterized plugins to your page using a simple callout
+        mechanism.
         ",
         
         #h2 { text="Data Binding" },
         #p{},
         "
-        Nitrogen leverages the power of Erlang data structures and pattern matching to 
-        enable powerful one-way databinding. Data binding support makes it easy to display repeated
-        documents like blog posts or comments.
+        Nitrogen leverages the power of Erlang data structures and pattern
+        matching to enable powerful one-way databinding. Data binding support
+        makes it easy to display repeated documents like blog posts or comments.
+        ",
+
+        #h2 { text="Infinitely Extendible" },
+        #p{},
+        "
+        With Nitrogen, you can create your own custom complex elements and
+        actions, then package them up as Nitrogen Plugins, to easily include in
+        other Nitrogen projects without having to copy-and-paste, or share them
+        with the world.
         ",
         
         #h2 { text="Erlang Power" },
         #p{},
         "
-        Nitrogen brings all of the advantages of Erlang to web application development,
-        including hot code swapping, stability, and scalability.
+        Nitrogen brings all of the advantages of Erlang to web application
+        development, including hot code swapping, stability, and scalability.
         ",
         
         #hr {},
         
         #h2 { text="Technology Stack" },
-        #h3 { text="Mac, Linux, Windows, etc." },
+        #h3 { text="Mac, Linux, Windows, FreeBSD, Raspberry Pi etc." },
         #p{},
         "   
-        Any platform that can   run Erlang can also run Nitrogen, including Mac, Linux, Unix, and Windows (among others). 
-        Nitrogen does not contain   any platform specific code. 
+        Any platform that can run Erlang can also run Nitrogen, including Mac,
+        Linux, Unix, and Windows (among others).  Nitrogen does not contain
+        any platform specific code. 
         ",
         
         #h3 { text="Mochiweb, Yaws, Cowboy, Webmachine, or Inets for Serving" },
         #p{},
         "
-        Nitrogen supports some of the most popular Erlang web servers equally: Mochiweb, Yaws, Cowboy, Webmachine, and Inets. 
-        Nitrogen abstracts out the server specific code, meaning that you write your application on 
-        one http server and seamlessly transfer to a different http server without changing a thing.
+        Nitrogen supports some of the most popular Erlang web servers equally:
+        Mochiweb, Yaws, Cowboy, Webmachine, and Inets.  Nitrogen abstracts out
+        the server specific code, meaning that you write your application on one
+        http server and seamlessly transfer to a different http server without
+        changing a thing.
         ",
         
-        #h3 { text="JQuery/JQuery UI for Javascript and Effects" },
+        #h3 { text="JQuery/JQuery UI/JQuery Mobile for Javascript and Effects" },
         #p{},
         "
-        Nitrogen uses JQuery and the JQuery UI library for client side Javascript and the JQuery Mobile library for mobile development.
+        Nitrogen uses JQuery and the JQuery UI library for client side
+        Javascript and the JQuery Mobile library for mobile development.
         "
     ].
 
@@ -131,7 +148,7 @@ right() ->
         
         #h2 { text="About Nitrogen" },
 
-    "
+        "
         <p>
         Nitrogen was created in 2008 by <a href='http://rusty.io'>Rusty
         Klophaus</a> (<a href='http://www.twitter.com/rustyio'>@rustyio</a>).
@@ -149,9 +166,7 @@ right() ->
         #h2 { text="Official Maintainers"},
         #p{},
         #list { body = [
-            lists:map(fun(C) ->
-                #listitem { body=format_contrib(C) }
-            end,maintainers())
+            [#listitem{body=format_contrib(C)} || C <- maintainers()]
         ] },
 
         #h2 { text="Thanks!" },
@@ -161,9 +176,7 @@ right() ->
 
         #p{},
         #list { body=[
-            lists:map(fun(C) ->
-                #listitem { body=format_contrib(C) }
-            end,thanks())
+            [#listitem{body=format_contrib(C)} || C <- thanks()]
         ] }
     ].
 
