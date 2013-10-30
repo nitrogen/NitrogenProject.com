4de8f4d90a534e9611450bd97c87f90dd5a09809
Jesse Gumm <sigmastar@gmail.com>
2013-10-30 08:26:56 -0500

    Add note about moving callout module

--- src/nitrogen_callout.erl
+++ src/nitrogen_callout.erl
@@ -6,6 +6,9 @@
 
 run(Bridge) ->
     nitrogen:init_request(Bridge),
-    %% Uncomment for basic authentication...
+
+	%% This file needs to be moved to nitrogen_core, with a handler callback provided.
+	%% Something like nitrogen_handlers, accompanied by perhaps a configuration file
+	%% pointing to the M:F() call.  For now, we'll keep it here
     nitrogen:handler(website_config_handler, []),
     nitrogen:run().
