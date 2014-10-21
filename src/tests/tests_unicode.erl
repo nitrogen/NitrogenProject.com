aacba309c258e7e05100a2f06085321caab11541
Jesse Gumm <gumm@sigma-star.com>
2014-10-21 16:43:34 -0500

    Unicode tests working

---
+++ src/tests/tests_unicode.erl
@@ -0,0 +1,84 @@
+-module(tests_unicode).
+-include_lib("nitrogen_core/include/wf.hrl").
+-compile(export_all).
+
+main() ->
+	wf_test:start(fun tests/0),
+	#template{file="templates/grid.html"}.
+
+which_string(StringNum) ->
+	N = wf:to_integer(wf:path_info()),
+	element(StringNum, lists:nth(N, strings())).
+		
+
+strings() ->
+	%% Miscellaneous translations from google translate
+	[
+		{"We're going to need a bigger boat", "Yes sir, the check is in the mail."},
+		{"نحن ذاهبون في حاجة إلى قارب أكبر", "نعم يا سيدي، والاختيار هو في البريد."},
+		{"我们将需要更大的船","是的，先生，支票在邮寄。"},
+		{"आम्ही एका मोठ्या बोट गरज आहोत.","सर होय, चेक मेल आहे."}, 
+		{"Budeme potřebovat větší loď","Ano, pane, kontrola je v e-mailu."},
+		{"Мы собираемся нуждаться в большей лодку","Да, сэр, проверка по почте."},
+		{"ჩვენ ვაპირებთ, რომ უნდა დიდი ნავი","დიახ სერ, შემოწმება ფოსტა."}
+	].
+
+
+title() -> which_string(1).
+
+layout() ->
+	#panel{id=wrapper, body=which_string(1)}.
+
+tests() ->
+	?wf_test_js(title_test, title_test()),
+	?wf_test_js(original_body_test, original_body_test()),
+	?wf_test_js(test_encoded, test_encoded()),
+	?wf_test_js(test_raw, test_raw()),
+	?wf_test_auto(add_unicode_textbox, add_unicode_textbox()),
+	?wf_test_auto(set_unicode_textbox_raw, set_unicode_textbox_raw()),
+	ok.
+
+title_test() ->
+	{
+		undefined,
+		"return $('title').text();",
+		fun([V]) -> V == "Nitrogen - " ++ which_string(1) end
+	}.
+
+original_body_test() ->
+	{
+		undefined,
+		"return objs('wrapper').text();",
+		fun([V]) -> V == which_string(1) end
+	}.
+
+test_encoded() ->
+	Str = which_string(2),
+	{
+		fun() -> wf:update(wrapper, wf:html_encode(Str)) end,
+		"return objs('wrapper').text();",
+		fun([V]) -> V == Str end
+	}.
+
+test_raw() ->
+	Str = which_string(2),
+	{
+		%% while test_encoded/0 encoded it with wf:html_encode, this just sends the raw unicode strings
+		fun() -> wf:update(wrapper, Str) end,
+		"return objs('wrapper').text();",
+		fun([V]) -> V == Str end
+	}.
+
+add_unicode_textbox() ->
+	Str = which_string(1),
+	{
+		fun() -> wf:insert_bottom(wrapper, #textbox{id=unicode, text=Str}) end,
+		fun() -> wf:q(unicode)==Str end
+	}.
+
+set_unicode_textbox_raw() ->
+	Str = which_string(2),
+	{
+		fun() -> wf:set(unicode, Str) end,
+		fun() -> wf:q(unicode)==Str end
+	}.
--- src/tests/tests_validation.erl
+++ src/tests/tests_validation.erl
@@ -8,19 +8,24 @@ main() ->
 tests() ->
 	%% suppress any alert boxes for this page
 	wf:wire("alert = function() { };"),
+	reset_values(),
 	?wf_test_js(blank_test, blank_test()),
 	?wf_test_js(fullname_test, fullname_test()),
 	?wf_test_js(allpass_test, allpass_test()),
 	?wf_test_js(passwordconfirm_test, passwordconfirm_test()),
 	?wf_test_js(shortpassword_test, shortpassword_test()),
 	?wf_test_js(badnumber_test, badnumber_test()),
-	?wf_test_js(number_out_of_range_test, number_out_of_range_test()).
+	?wf_test_js(number_out_of_range_test, number_out_of_range_test()),
+	?wf_test_js(unicode_value_test, unicode_value_test()),
+	?wf_test_js(unicode_comparison_test, unicode_comparison_test()),
+	?wf_test_js(unicode_comparison_pass_test, unicode_comparison_pass_test()),
+	?wf_test_js(unicode_validator_test, unicode_validator_test()).
 
 
 -define(click, wf:wire(continueButton, #click{})).
 -define(count, "return $('.LV_validation_message').length;").
 -define(set(FV), fun() -> set(FV), ?click end).
--define(opts, [{delay, 100}]).
+-define(opts, [{delay, 300}]).
 
 blank_test() ->
 	{
@@ -107,13 +112,91 @@ number_out_of_range_test() ->
 			{nameTextBox, "rusty lastname"},
 			{emailTextBox, "email@email.com"},
 			{passwordTextBox, "password"},
-			{confirmTextBox, "passwordother"},
+			{confirmTextBox, "password"},
 			{numberTextBox, "11"} %% range should be 1-10
 		]),
 		?count,
 		fun([Length]) -> Length == 1 end,
 		?opts
 	}.
+
+
+unicode_value_test() ->
+	{
+		?set([
+			{nameTextBox, "Тест ыыы"},
+			{emailTextBox, "email@email.com"},
+			{passwordTextBox, "password"},
+			{confirmTextBox, "password"},
+			{numberTextBox, "10"} %% range should be 1-10
+		]),
+		?count,
+		fun([Length]) -> Length == 1 end,
+		?opts
+	}.
+
+unicode_comparison_test() ->
+	{
+		fun() ->
+			wf:wire(continueButton, otherTextBox, #validate{validators=[
+				#custom{
+					text="Must start with 'Тест'",
+					tag=whatever,
+					function=fun(_, V) ->
+								case V of 
+									"Тест" ++ _ -> true;
+									_ -> false
+								end
+							 end
+				}
+			]}),
+			?set([
+				{otherTextBox, "not-valid"},
+				{nameTextBox, "rusty lastname"},
+				{emailTextBox, "email@email.com"},
+				{passwordTextBox, "password"},
+				{confirmTextBox, "password"},
+				{numberTextBox, "10"} %% range should be 1-10
+			])()
+		end,
+		?count,
+		fun([Length]) -> Length == 1 end,
+		?opts
+	}.
+
+unicode_comparison_pass_test() ->
+	{
+		?set([
+			{otherTextBox, "Тест blahblah"},
+			{nameTextBox, "rusty klophaus"},
+			{emailTextBox, "email@email.com"},
+			{passwordTextBox, "password"},
+			{confirmTextBox, "password"},
+			{numberTextBox, "10"} %% range should be 1-10
+		]),
+		?count,
+		fun([Length]) -> Length == 0 end,
+		?opts
+	}.
+
+unicode_validator_test() ->
+	{
+		fun() ->
+			wf:eager(continueButton, otherTextBox, #validate{validators=#is_required{text="Тест ыыы"}}),
+			?set([
+				{otherTextBox, ""},
+				{nameTextBox, "rusty lastname"},
+				{emailTextBox, "email@email.com"},
+				{passwordTextBox, "password"},
+				{confirmTextBox, "password"},
+				{numberTextBox, "10"} %% range should be 1-10
+			])()
+		end,
+		?count,
+		fun([Length]) -> Length == 1 end,
+		?opts
+	}.
+
 
 
 set(FieldValues) ->	
