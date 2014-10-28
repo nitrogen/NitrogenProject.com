c756a4d163b7be0a19d11503a2161face41dc7e1
Jesse Gumm <gumm@sigma-star.com>
2014-10-28 15:53:57 -0500

    Add template unicode test

---
+++ src/tests/tests_unicode_template.erl
@@ -0,0 +1,19 @@
+-module(tests_unicode_template).
+-include_lib("nitrogen_core/include/wf.hrl").
+-compile(export_all).
+
+main() ->
+	wf_test:start(fun tests/0),
+	#template{file="templates/unicode-test.html"}.
+
+tests() ->
+	?wf_test_js(unicode_template_text, unicode_template("text")),
+	?wf_test_js(unicode_template_html, unicode_template("html")),
+	ok.
+
+unicode_template(Getter) ->
+	{
+		undefined,
+		"return $('#value')." ++ Getter ++ "();",
+		fun([V]) -> V == "Δοκιμή Πρότυπο" end
+	}.
