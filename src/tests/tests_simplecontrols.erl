6997a004523223488e02a6dfef5a131fdc73f12d
Jesse Gumm <gumm@sigma-star.com>
2014-10-30 01:58:55 -0500

    Add tests for blank controls

--- src/tests/tests_simplecontrols.erl
+++ src/tests/tests_simplecontrols.erl
@@ -6,22 +6,24 @@ main() ->
 	wf_test:start_other(demos_simplecontrols, fun tests/0).
 
 tests() ->
-    ?wf_test_auto(textbox, test_textbox()),
+    ?wf_test_auto(textbox, test_textbox("Test Value")),
+    ?wf_test_auto(blank_textbox, test_textbox("")),
     ?wf_test_auto(textarea, test_textarea()),
     ?wf_test_auto(dropdown, test_dropdown()),
+    ?wf_test_auto(blank_dropdown, test_blank_dropdown()),
     ?wf_test_auto(radio, undefined, fun check_radio/0),
     ?wf_test_auto(checkbox, test_checkbox()),
     ?wf_test_auto(checkbox_changed, test_checkbox_changed()),
     ?wf_test_auto(insert_after_order, test_insert_after_order()),
+    ?wf_test_auto(blank_multi_dropdown, test_multidropdownblank()),
     ?wf_test_auto(multi_dropdown, test_multidropdown()),
     ?wf_test_auto(multi_dropdown1, test_multidropdown1()),
     ?wf_test_manual(click_postback, test_remove_multi_click_postback()).
 
-test_textbox() ->
-    Str = "Some new val",
+test_textbox(Str) ->
     {
         fun() -> wf:set(textbox, Str) end,
-        fun() -> wf:q(textbox)== Str end
+        fun() -> wf:q(textbox) == Str end
     }.
 
 test_textarea() ->
@@ -36,6 +38,12 @@ test_dropdown() ->
         fun() -> wf:set(my_dropdown, "opt2") end,
         fun() -> wf:q(my_dropdown) == "opt2" end
     }.
+
+test_blank_dropdown() ->
+	{
+		fun() -> wf:set(my_dropdown, "invalid") end,
+		fun() -> wf:q(my_dropdown) == "" end
+	}.
 
 test_checkbox() ->
     {
@@ -59,6 +67,12 @@ test_insert_after_order() ->
         fun() ->
             wf:qs(order1) == ["3","2","1"]
         end
+    }.
+
+test_multidropdownblank() ->
+    {
+        fun() -> wf:set_multiple(multiple, []) end,
+        fun() -> wf:qs(multiple) == [] end
     }.
 
 test_multidropdown() ->
