65fdd8c4d2b4b1f3616be1b9562a46c40a6cceff
Jesse Gumm <gumm@sigma-star.com>
2020-11-25 12:20:08 -0600

    Add github link to menu

--- src/common.erl
+++ src/common.erl
@@ -17,12 +17,13 @@ platform() ->
 header(Selected) ->
     wf:wire(Selected, #add_class { class=selected }),
     #panel { class=menu, body=[
-        #link { id=home, url='/', text="HOME" },
-        #link { id=downloads, url='/downloads', text="DOWNLOADS" },
-        #link { id=demos, url='/demos', text="DEMOS" },
-        #link { id=docs, url='/doc/index', text="DOCUMENTATION" },
-        #link { id=learn, url='/learn', text="LEARN MORE" },
-        #link { id=community, url='/community', text="GET INVOLVED" }
+        #link { id=home, url="/", text="HOME" },
+        #link { id=downloads, url="/downloads", text="DOWNLOADS" },
+        #link { id=demos, url="/demos", text="DEMOS" },
+        #link { id=docs, url="/doc/index", text="DOCUMENTATION" },
+        #link { id=learn, url="/learn", text="LEARN MORE" },
+        #link { id=community, url="/community", text="GET INVOLVED" },
+        #link { id=source, url="https://github.com/nitrogen/nitrogen", text="GITHUB"}
     ]}.
 
 footer() ->
