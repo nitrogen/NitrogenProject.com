690419ccca6c99edaadb1acbff73e186078759aa
Jesse Gumm <gumm@sigma-star.com>
2024-01-09 19:35:32 -0600

    Add update to profile stuff, but disable profiling by default

--- src/nitrogen_main_handler.erl
+++ src/nitrogen_main_handler.erl
@@ -1,4 +1,5 @@
 -module(nitrogen_main_handler).
+-include_lib("nitrogen_core/include/wf.hrl").
 -export([
         run/0,
         ws_init/0
@@ -14,3 +15,15 @@ ws_init() ->
 run() ->
     handlers(),
     wf_core:run().
+    %?WF_PROFILE(run, wf_core:run(), csv_path()).
+   
+csv_path() ->
+    filename:join(home_dir(), profile_file()).
+
+profile_file() ->
+    DateTime = qdate:to_string("Y-m-d-h-i-s"),
+    "nitrogen_profile-" ++ DateTime ++ ".csv".
+
+home_dir() ->
+    {ok, [[Home]] = init:get_argument(home),
+     Home.
