7ad8d05982b3eb3c6d8db38a864b2d964e7496cb
Jesse Gumm <gumm@sigma-star.com>
2025-10-30 18:07:07 -0500

    Rework the demos page to use iteration

--- src/demos.erl
+++ src/demos.erl
@@ -39,86 +39,106 @@ left() ->
         "
     ].
 
+
 middle() -> 
-    [
-        #h2 { text="Controls and Validation" },
-        #p{},
-        #link { text="Simple Controls", url="/demos/simplecontrols" }, #br{}, 
-        #link { text="Limiting Postbacks with Vessels", url="/demos/vessel"}, #br{},
-        #link { text="In-Place Textbox", url="/demos/advancedcontrols1" }, #br{}, 
-        #link { text="Google Charts", url="/demos/advancedcontrols2" }, #br{}, 
-        #link { text="Mermaid Flowcharts and Diagrams", url="/demos/mermaid1" },#br{},
-        #link { text="QR Codes", url="/demos/qr" }, #br{}, 
-        #link { text="Sparkline", url="/demos/sparkline" },#br{},
-        #link { text="Radio Buttons", url="/demos/radio" }, #br{}, 
-        #link { text="File Uploading", url="/demos/upload" }, #br{}, 
-        #link { text="User Notices", url="/demos/notices" }, #br{}, 
-        #link { text="Validation", url="/demos/validation" }, #br{},
-        #link { text="Dynamically Removing Validation", url="/demos/clear_validation"}, #br{},
-        #link { text="Autocompletion", url="/demos/textbox_autocomplete" }, #br{}, 
-        #link { text="Wizard", url="/demos/wizard" },#br{},
-        #link { text="RESTful Forms", url="/demos/restful" }, #br{},
-        #link { text="HTML and Custom Encoding", url="/demos/htmlencode"},#br{},
-%        #link { text="Recaptcha", url="/demos/recaptcha"},#br{},
-
-        #h2 { text="Drag, Drop & Sort" },
-        #p{},
-        #link { text="Drag and Drop", url="/demos/dragdrop" }, #br{}, 
-        #link { text="Sorting", url="/demos/sorting1" }, #br{}, 
-        #link { text="Nested Sorting", url="/demos/sorting2" }, #br{},
-
-        #h2 { text="Data Binding" },
-        #p{},
-        #link { text="Simple (List-Based) Binding", url="/demos/binding1" }, #br{}, 
-        #link { text="Record-Based Binding", url="/demos/binding2" }, #br{}, 
-        #link { text="Key/Value Pair Binding", url="/demos/binding3" }, #br{}, 
-        #link { text="Binding With a Transform Function", url="/demos/binding4" }, #br{}
-    ].
+    Demos = [
+        {"Controls and Validation", [
+            {"Simple Controls","/demos/simplecontrols"},
+            {"Limiting Postbacks with Vessels","/demos/vessel"},
+            {"In-Place Textbox","/demos/advancedcontrols1"},
+            {"Radio Buttons","/demos/radio"},
+            {"File Uploading","/demos/upload"},
+            {"User Notices","/demos/notices"},
+            {"Validation","/demos/validation"},
+            {"Dynamically Removing Validation","/demos/clear_validation"},
+            {"Autocompletion","/demos/textbox_autocomplete"},
+            {"Wizard","/demos/wizard"},
+            {"RESTful Forms","/demos/restful"},
+            {"HTML and Custom Encoding","/demos/htmlencode"}
+            %{"Recaptcha", "/demos/recaptcha"},
+        ]},
+        {"Charts and Graphs", [
+            {"Google Charts","/demos/advancedcontrols2"},
+            {"Mermaid Flowcharts and Diagrams","/demos/mermaid1"},
+            {"Mermaid with Postbacks","/demos/mermaid2"},
+            {"Mermaid Async Updates","/demos/mermaid3"},
+            {"QR Codes","/demos/qr"},
+            {"Sparkline","/demos/sparkline"}
+        ]},
+        {"Drag, Drop & Sort", [
+            {"Drag and Drop","/demos/dragdrop"},
+            {"Sorting","/demos/sorting1"},
+            {"Nested Sorting","/demos/sorting2"}
+        ]},
+        {"Data Binding", [
+            {"Simple (List-Based) Binding","/demos/binding1"},
+            {"Record-Based Binding","/demos/binding2"},
+            {"Key/Value Pair Binding","/demos/binding3"},
+            {"Binding With a Transform Function","/demos/binding4"}
+        ]}
+    ],
+    draw_demos(Demos).
 
 right() ->
+    Demos = [
+        {"Events and Ajax",[
+            {"Effects","/demos/effects" },
+            {"Postbacks","/demos/postback" },
+            {"Postbacks with Form Fields","/demos/postback2" },
+            {"Priority Wiring","/demos/priority_wiring"},
+            {"Javascript Conditional Events","/demos/conditionals"},
+            {"AJAX Updates","/demos/ajax" },
+            {"AJAX Replace","/demos/replace" },
+            {"AJAX Remove","/demos/remove" },
+            {"Spinner","/demos/spinner" },
+            {"JQuery Paths","/demos/jquerypaths" },
+            {"Javascript API","/demos/api" },
+            {"Modal Popups","/demos/modal" }
+        ]},
+
+        {"Comet/Asynchronous Calls",[
+            {"Counter with Comet","/demos/comet1" },
+            {"Chatroom with Comet","/demos/comet2" },
+            {"Multiple Comets with Graceful Exit","/demos/comet3" },
+            {"Continuations","/demos/continuations" },
+            {"Delayed Content (with some caching)","/demos/delay_body"},
+            {"Progress Bars","/demos/progress_bar" },
+            {"Synchronized Panels (using comet)","/demos/sync_panel" },
+            {"Custom Websocket Functionality","/demos/websocket"}
+        ]},
+    
+        %{"Mobile Integration",[
+        %    {"Side Panel", "/demos/mobile_panel"},
+        %    {"Mobile Lists","/demos/mobile_list"},
+        %    {"Mobile Controls with Postbacks","/demos/mobile_controls"},
+        %    {"Mobile Collapsibles","/demos/mobile_collapsibles"},
+        %    {"Dynamically adding Mobile Elements","/demos/mobile_controls2"}
+        %]},
+        {"Advanced Topics",[
+            {"Optimized Coalescing with lazy_coalesce", "/demos/coalesce"},
+            {"Set Content Type and Response Headers","/demos/contenttype" },
+            {"Rest API Handler","/demos/rest"},
+            {"Smart Extensions (JSON, CSV, etc)","/demos/smart_extensions" },
+            {"Request HTTP Headers","/demos/headers" },
+            {"Security (User management)","/demos/security" },
+            {"State (Page and Session)","/demos/state"}
+        ]}
+    ],
+    draw_demos(Demos).
+
+
+draw_demos(Demos) ->
+    [draw_demo_section(Sec) || Sec <- Demos].
+
+draw_demo_section({Title, Links}) ->
     [
-        #h2 { text="Events and Ajax" },
-        #p{},
-        #link { text="Effects", url="/demos/effects" }, #br{}, 
-        #link { text="Postbacks", url="/demos/postback" }, #br{}, 
-        #link { text="Postbacks with Form Fields", url="/demos/postback2" }, #br{}, 
-        #link { text="Priority Wiring", url="/demos/priority_wiring"}, #br{},
-        #link { text="Javascript Conditional Events", url="/demos/conditionals"}, #br{},
-        #link { text="AJAX Updates", url="/demos/ajax" }, #br{}, 
-        #link { text="AJAX Replace", url="/demos/replace" }, #br{},
-        #link { text="AJAX Remove", url="/demos/remove" }, #br{},
-        #link { text="Spinner", url="/demos/spinner" }, #br{},
-        #link { text="JQuery Paths", url="/demos/jquerypaths" }, #br{}, 
-        #link { text="Javascript API", url="/demos/api" }, #br{},
-        #link { text="Modal Popups", url="/demos/modal" },#br{},
-        #link { text="Mermaid with Postbacks", url="/demos/mermaid2" },#br{},
+        #h2{text=Title},
+        draw_demo_links(Links)
+    ].
 
-        #h2 { text="Comet/Asynchronous Calls" },
-        #p{},
-        #link { text="Counter with Comet", url="/demos/comet1" }, #br{},
-        #link { text="Chatroom with Comet", url="/demos/comet2" }, #br{},
-        #link { text="Multiple Comets with Graceful Exit", url="/demos/comet3" }, #br{},
-        #link { text="Continuations", url="/demos/continuations" }, #br{},
-        #link { text="Delayed Content (with some caching)", url="/demos/delay_body"}, #br{},
-        #link { text="Progress Bars", url="/demos/progress_bar" },#br{},
-        #link { text="Synchronized Panels (using comet)", url="/demos/sync_panel" },#br{},
-        #link { text="Mermaid Async Updates", url="/demos/mermaid3" },#br{},
-        #link { text="Custom Websocket Functionality", url="/demos/websocket"},#br{},
+draw_demo_links(Links) ->
+    RenderedLinks = [draw_demo_link(Link) || Link <- Links],
+    wf:join(RenderedLinks, #br{}).
 
-        %#h2 { text="Mobile Integration" },
-        %#p{},
-        %#link { text="Side Panel", url="/demos/mobile_panel" },#br{},
-        %#link { text="Mobile Lists", url="/demos/mobile_list" },#br{},
-        %#link { text="Mobile Controls with Postbacks", url="/demos/mobile_controls" },#br{},
-        %#link { text="Mobile Collapsibles", url="/demos/mobile_collapsibles" },#br{},
-        %#link { text="Dynamically adding Mobile Elements", url="/demos/mobile_controls2" },#br{},
-
-        #h2 { text="Advanced Topics" },
-        #p{},
-        #link { text="Set Content Type and Response Headers", url="/demos/contenttype" }, #br{},
-        #link { text="Rest API Handler", url="/demos/rest"}, #br{},
-        #link { text="Smart Extensions (JSON, CSV, etc)", url="/demos/smart_extensions" }, #br{},
-        #link { text="Request HTTP Headers", url="/demos/headers" }, #br{},
-        #link { text="Security (User management)", url="/demos/security" }, #br{},
-        #link { text="State (Page and Session)", url="/demos/state" }
-    ].  
+draw_demo_link({Text, Url}) ->
+    #link{text=Text, url=Url}.
